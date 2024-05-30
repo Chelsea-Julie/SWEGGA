25287728c5e6e8bd9ffb252cb2cc722641fff49e
Chelsea-Julie <chelseaqjulie.gmail.com>
2024-05-30 16:42:27 +0200

    Final Product

---
+++ scrip/code.js
@@ -0,0 +1,26 @@
+let entry1 = document.querySelector(".entry1") 
+let result = document.querySelector(".result")
+let result2 = document.querySelector(".result2")
+let entry2 = document.querySelector(".entry2") 
+let grandtotal = document.querySelector(".grandtotal")
+let final = document.querySelector(".final")
+
+let numb1;
+let numb2;
+
+entry1.addEventListener("change", () =>{
+    answer = 150.95 * (+entry1.value)
+   result.innerText =   answer.toFixed(2)
+   
+
+})
+
+entry2.addEventListener("change", () =>{
+    answer = 180.95 * (+entry2.value)
+    result2.innerText =   answer.toFixed(2)
+ 
+ })
+
+grandtotal.addEventListener("click", () =>{
+    final.innerText = (numb1 + numb2).toFixed(2)
+})
